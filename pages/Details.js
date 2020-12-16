9c6421b632f9f7bc338bc2272572668808978abd
vmnbhuvani7 <31824451+vmnbhuvani7@users.noreply.github.com>
2020-12-16 09:53:30 +0530

    update

--- pages/Details.js
+++ pages/Details.js
@@ -1,24 +1,39 @@
+import Axios from 'axios'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 const people = [
     { v: "Car", name: 'Bruno' },
     { v: "Bike", name: 'John' },
     { v: "Airplane", name: 'Mick' }
 ]
 
-export default function Details() {
-    return <div>
-        {people.map(item => (
-            <>
-                <Link as={`/${item.v}/${item.name}`} href="/[keval]/[kels]">
-                    <a >Navigation to {item.name}'s {item.v}<br></br></a>
+export default function Details({ ownerList }) {
+    // const [owners, setOwners] = useState([])
+    // useEffect(() => {
+    //     async function loadData() {
+    //         const response = await Axios.get('http://localhost:8000/')
+    //         const ownerList = await response.json()
+    //         setOwners(ownerList);
+    //     }
+    //     loadData();
+    // }, [])
 
-                </Link>
-                <Link as={`/${item.v}/${item.name}`} href="/[vehicle]/[person]">
-                    <a >Navigation to {item.name}'s {item.v}<br></br></a>
+    return <div>
+        {ownerList.map(item => (
+            <>
+                {console.log("tii", item)}
+                <Link as={`/${item.vehicle}/${item.ownerName}`} href="/[vehicle]/[person]">
+                    <a >Navigation to {item.ownerName}'s {item.vehicle}<br></br></a>
 
                 </Link>
             </>
         ))}
 
     </div>
+}
+
+Details.getInitialProps = async () => {
+    const response = await fetch('http://localhost:8000/')
+    const ownerList = await response.json()
+    return { ownerList: ownerList }
 }
--- pages/[vehicle]/[person].js
+++ pages/[vehicle]/[person].js
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router'
 export default function Person() {
     const router = useRouter();
-    console.log(router.query);
     return <h1>{router.query.person}'s {router.query.vehicle}</h1>
 }
